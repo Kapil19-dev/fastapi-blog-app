2aae5fdef58cbf62eb247bf1497e17a9b18c9eff
Kapil19-dev <rsingh2@student.nitw.ac.in>
2023-11-27 10:54:19 +0530

    Initial Commit

--- backend/db/session.py
+++ backend/db/session.py
@@ -1,10 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from ..core.config import settings
+from pydantic import PostgresDsn
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-
-#print("database url is", SQLALCHEMY_DATABASE_URL)
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SESSIONLOCAL = sessionmaker(autoflush= False, autocommit=False, bind=engine)
+SQLALCHEMY_DATABASE_URI = PostgresDsn.build(
+    scheme="postgresql",
+    user="POSTGRES_USER",
+    password="POSTGRES_PASSWORD",
+    host="POSTGRES_SERVER",
+    path=f"/{'POSTGRES_DB' or ''}",
+)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    pool_pre_ping=True,
+)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+try:
+    db = SessionLocal()
+    db.execute("SELECT 1")
+except Exception as e:
+    raise e
